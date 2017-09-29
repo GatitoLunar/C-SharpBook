198b8588c1f2b98490370b3e93851bd206c99271
GatitoLunar <lunothesoldier@gmail.com>
2017-09-29 15:41:06 -0300

    Update GlazerCalc.cs

--- GlazerCalc.cs
+++ GlazerCalc.cs
@@ -5,19 +5,57 @@ class GlazerCalc
 	static void Main()
 	{
 		double width, height, woodLength, glassArea;
+		
+		const double MAX_WIDTH = 5.0 ;
+		const double MIN_WIDTH = 0.5 :
+		const double MAX_HEIGHT = 3.0 ;
+		const double MIN_HEIGHT = 0.75 ;
+		
 		string widthString, heightString;
 		
+		Console.Write( "Give the width of the window : ");
 		widthString = Console.ReadLine();
 		width = double.Parse(widthString);
 		
+		if (width > MAX_WIDTH)
+		{
+			Console.WriteLine("Width is too large \n\n");
+			Console.WriteLine("Using maximum");
+			width = MAX_WIDTH;
+		}
+		
+		if (width < MIN_WIDTH)
+		{
+			Console.WriteLine("Width is too small \n\n");
+			Console.WriteLine("Using minimum");
+			width = MIN_WIDTH;
+		}
+		
+		Console.Write("Give the height of the window : ");
 		heightString = Console.ReadLine();
 		height = double.Parse(heightString);
 		
-		woodLength = 2 * (width + height) * 3.25;
+		if (height > MAX_HEIGHT)
+		{
+			Console_WriteLine("Height is too large \n\n");
+			Console_WriteLine("Using maximum");
+			height = MAX_HEIGHT;
+		}
 		
-		glassArea = 2 * (width * height);
+		if (height < MIN_HEIGHT)
+		{
+			Console_WriteLine("Height is too small \n\n");
+			Console_WriteLine("Using minimum");
+			height = MIN_HEIGHT;
+		}
 		
-		Console.WriteLine("The length of the wood is " + woodLength + " feets");
-		Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+		woodLength = 2 * ( width + height ) * 3.25 ;
+		
+		glassArea = 2 * ( width * height ) ;
+		
+		Console.WriteLine("The length of the wood is " +
+				 woodLength + " feet");
+		Console.WriteLine("The area of the glass is " +
+				 glassArea + " square metres");
 	}
 }
